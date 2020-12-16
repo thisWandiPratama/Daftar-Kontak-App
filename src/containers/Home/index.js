d3ea1367e7cfcdb817707098929a4135a81f2c1a
thisWandiPratama <wandi99pratama@gmail.com>
2020-12-16 18:56:29 +0700

    Add GET DATA dan UI Home

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,21 +1,58 @@
 //import liraries
-import React, { Component } from 'react';
-import { View, Text, TouchableOpacity} from 'react-native';
-import { styles } from "../../styles/_home";
-import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
-
+import React, {useState, useEffect} from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
+import {styles} from '../../styles/_home';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faPlus, faUser} from '@fortawesome/free-solid-svg-icons';
+import FIREBASE from '../../configs/FIREBASE';
+import Header from '../../components/headers';
 
 // create a component
 const Home = ({navigation}) => {
-    return (
-        <View style={styles.container}>
-            <Text>Home</Text>
-            <TouchableOpacity style={styles.btnAdd} onPress = { () => navigation.navigate('Add') }>
-                <FontAwesomeIcon icon={faPlus} size= {20} color='#FFFF' />
-            </TouchableOpacity>
-        </View>
-    );
+  const [kontaks, setKontaks] = useState ({});
+  const [kontaksKey, setKontaksKey] = useState ([]);
+
+  useEffect (() => {
+    getData ();
+    setTimeout (() => {
+      console.log (kontaks);
+      console.log (kontaksKey);
+    }, 5000);
+  }, []);
+
+  const getData = () => {
+    FIREBASE.database ().ref ('Kontak').once ('value', querySnapShot => {
+      let data = querySnapShot.val () ? querySnapShot.val () : {};
+      let kontakItem = {...data};
+      setKontaks (kontakItem);
+      setKontaksKey (Object.keys (kontakItem));
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Header title="Daftar Kontak" onPress={() => navigation.goBack ()} />
+      {kontaksKey.length > 0
+        ? kontaksKey.map (key => (
+            <View style={styles.kontak} key={key}>
+              <FontAwesomeIcon icon={faUser} size={25} color="blue" />
+              <View>
+                <Text style={styles.nama}> {kontaks[key].nama} </Text>
+                <Text style={styles.nomor}> {kontaks[key].no_hp} </Text>
+              </View>
+            </View>
+          ))
+        : <View style={styles.kontakKosong}>
+            <Text style={styles.kosong}>Daftar Kosong</Text>
+          </View>}
+      <TouchableOpacity
+        style={styles.btnAdd}
+        onPress={() => navigation.navigate ('Add')}
+      >
+        <FontAwesomeIcon icon={faPlus} size={20} color="#FFFF" />
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 //make this component available to the app
--- src/styles/_home.js
+++ src/styles/_home.js
@@ -1,24 +1,41 @@
-import { StyleSheet } from "react-native";
+import {StyleSheet} from 'react-native';
 
-const styles  = StyleSheet.create({
-    container : {
-        flex : 1
-    },
-    btnAdd : {
-        height : 50,
-        width : 50,
-        backgroundColor : 'skyblue',
-        alignItems : 'center',
-        justifyContent : 'center',
-        borderRadius : 30,
-        position : 'absolute',
-        bottom : 0,
-        right : 0,
-        marginRight : 20,
-        marginBottom :20
-    }
-})
+const styles = StyleSheet.create ({
+  container: {
+    flex: 1,
+  },
+  btnAdd: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'skyblue',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30,
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    marginRight: 20,
+    marginBottom: 20,
+  },
+  kontak: {
+    height: 50,
+    margin: 10,
+    flexDirection : 'row',
+    alignItems:'center'
+  },
+  nama: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  nomor : {
+    fontSize : 12,
+    color : 'rgba(0,0,0,0.5)'
+  },
+  kontakKosong: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
-export { 
-    styles
-}
+export {styles};
