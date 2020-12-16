b25a0a2e5920f119a2d9eb6fba288b12fce36c20
thisWandiPratama <wandi99pratama@gmail.com>
2020-12-16 20:03:46 +0700

    Add Delete Kontak

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,6 +1,6 @@
 //import liraries
 import React, {useState, useEffect} from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, Alert} from 'react-native';
 import {styles} from '../../styles/_home';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {
@@ -11,6 +11,7 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import FIREBASE from '../../configs/FIREBASE';
 import Header from '../../components/headers';
+import { set } from 'react-native-reanimated';
 
 // create a component
 const Home = ({navigation}) => {
@@ -19,10 +20,6 @@ const Home = ({navigation}) => {
 
   useEffect (() => {
     getData ();
-    setTimeout (() => {
-      console.log (kontaks);
-      console.log (kontaksKey);
-    }, 5000);
   }, []);
 
   const getData = () => {
@@ -32,6 +29,32 @@ const Home = ({navigation}) => {
       setKontaks (kontakItem);
       setKontaksKey (Object.keys (kontakItem));
     });
+  };
+
+  const removeKontak = key => {
+    Alert.alert (
+      'Konfirmasi',
+      'Anda yakin ingin menghapus kontak',
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log ('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {
+          text: 'OK',
+          onPress: () => {
+            FIREBASE.database ().ref ('Kontak/' + key).remove ();
+            setTimeout (() => {
+                setKontaksKey([])
+              getData ();
+            }, 3000);
+            // Alert.alert ('Hapus', 'Sukses Hapus Kontak');
+          },
+        },
+      ],
+      {cancelable: false}
+    );
   };
 
   return (
@@ -71,7 +94,7 @@ const Home = ({navigation}) => {
                   <TouchableOpacity>
                     <FontAwesomeIcon icon={faEdit} size={20} color="#434" />
                   </TouchableOpacity>
-                  <TouchableOpacity>
+                  <TouchableOpacity onPress={() => removeKontak (key)}>
                     <FontAwesomeIcon icon={faTimes} size={20} color="#434" />
                   </TouchableOpacity>
                 </View>
